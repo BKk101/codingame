a18b0b16b8e8deaf8086aab79872c6c69bd65dd8
BK <bk@MacBook-Pro.local>
2020-04-16 01:55:45 +0900

    edit

--- strategy1.cpp
+++ strategy1.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-
+#define POLE_R 600
 using namespace std;
 
 struct Entity
@@ -23,11 +23,16 @@ struct Pos
 	int y;
 };
 
-void Patrol(int pos, vector <Entity> list, int i);
-void Throw(int pos);
-void Spell(int pos, int mp, vector <Entity> snaffles, Pos des);
-vector <Entity> find_object(vector <Entity> list, int entities, char* obj);
+void Patrol(Pos start, Pos end, Pos wizard, int thrust);
+void Throw(Pos des, int power);
+void Spell(int mp, int Id, Pos des);
+void Move(int x, int y, int thrust);
+vector <Entity> find_object(vector <Entity> list, int entities, const char* obj);
 double Distance(int x1, int y1, int x2, int y2); 
+int near_des(vector <Entity> units, Pos des);
+int far_des(vector <Entity> units, Pos des);
+bool cmp(const Entity& a, const Entity& b);
+int in_bound(Pos obj, vector <Entity> units, int range);
 
 int main()
 {
@@ -52,34 +57,43 @@ int main()
 			>> entity_list[i].state; cin.ignore();
         }
 		vector <Entity> snaffles(find_object(entity_list, entities, "SNAFFLE"));
+		vector <Entity> wizards(find_object(entity_list, entities, "WIZARD"));
+		vector <Entity> op_wizards(find_object(entity_list, entities, "OPPONENT_WIZARD"));
+		Pos my_goal, op_goal;
 		if (myTeamId == 0) {
-			Pos op_goal = {16000, 3750};
-			Pos my_goal = {0, 3750};
+			op_goal.x = 16000; op_goal.y = 3750;
+			my_goal.x = 0; my_goal.y = 3750;
 		}
 		else {
-			Pos op_goal = {0, 3750};
-			Pos my_goal = {16000, 3750};
+			op_goal.x = 0; op_goal.y = 3750;
+			my_goal.x = 16000; my_goal.y = 3750;
 		}
+		Pos wizard0_pos = {wizards[0].x, wizards[0].y};
+		Pos wizard1_pos = {wizards[1].x, wizards[1].y};
         for (int i = 0; i < 2; i++) {
 			//wizard0 command
-			if (entity_list[i].state == 1) {
-					Throw(myTeamId);
-					continue;
-			}
             if (i == 0) {
-				cout << "MOVE 8000 3750 100" << endl;
+				if (wizards[0].state == 1) {
+					Throw(op_goal, 500);
+					continue;
+				}
+				if (myMagic > 30) {
+					Spell(myMagic, near_des(snaffles, op_goal), op_goal);
+					continue;
+				}
+				Pos p1 = {8000, 1000};
+				Pos p2 = {8000, 6500};
+				Patrol(p1, p2, wizard0_pos, 150);
 			}
 			//wizard1 command
 			else if (i == 1) {
-				if (myMagic > 50) {
-					Spell(myTeamId, myMagic, snaffles, op_goal);
+				if (in_bound(wizard1_pos, snaffles, 1000)) {
+					Spell(myMagic, near_des(snaffles, wizard1_pos), op_goal);
 					continue;
 				}
-				if (entity_list[i].state == 1) {
-					Throw(myTeamId);
-					continue;
-				}
-				Patrol(myTeamId, entity_list, i);
+				Pos p3 = {my_goal.x + 800, (my_goal.y - 2000)};
+				Pos p4 = {my_goal.x + 800, (my_goal.y + 2000)};
+				Patrol(p3, p4, wizard1_pos, 150);
 			}
             // To debug: cerr << "Debug messages..." << endl;
         }
@@ -91,7 +105,12 @@ double Distance(int x1, int y1, int x2, int y2)
 	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
-vector <Entity> find_object(vector <Entity> list, int entities, char* obj)
+bool cmp(const Entity& a, const Entity& b)
+{
+    return (a.entityId < b.entityId);
+}
+
+vector <Entity> find_object(vector <Entity> list, int entities, const char* obj)
 {
 	int cnt = 0;
 	for (int i=0;i<entities;i++) {
@@ -104,83 +123,79 @@ vector <Entity> find_object(vector <Entity> list, int entities, char* obj)
 			object.push_back(list[i]);
 		}
 	}
+	sort(object.begin(), object.end(), cmp);
 	return object;
 }
 
-void Spell(int pos, int mp, vector <Entity> snaffles, Pos des)
+int in_bound(Pos obj, vector <Entity> units, int range)
+{
+	for (int i=0;i<units.size();i++) {
+		if (Distance(obj.x, obj.y, units[i].x, units[i].y) <= range)
+			return 1;
+	}
+	return 0;
+}
+
+int near_des(vector <Entity> units, Pos des)
 {
 	int dis = 16000;
 	int nearId;
-	for (int i=0;i<snaffles.size();i++) {
-		if (Distance(snaffles[i].x, snaffles[i].y, des.x, des.y) <= dis) {
-			dis = Distance(snaffles[i].x, snaffles[i].y, des.x, des.y);
-			nearId = snaffles[i].entityId;
+	for (int i=0;i<units.size();i++) {
+		if (Distance(units[i].x, units[i].y, des.x, des.y) <= dis) {
+			dis = Distance(units[i].x, units[i].y, des.x, des.y);
+			nearId = units[i].entityId;
 		}
 	}
-	cout << "WINGARDIUM "<<nearId<<" "<<des.x<<" "<<des.y<<" "<<50<< endl;
+	return nearId;
 }
 
-void Throw(int pos)
+int far_des(vector <Entity> units, Pos des)
 {
-	if (pos == 0)
-		cout << "THROW 16000 3750 500" << endl;
-	else
-		cout << "THROW 0 3750 500" << endl;
+	int dis = 0;
+	int farId;
+	for (int i=0;i<units.size();i++) {
+		if (Distance(units[i].x, units[i].y, des.x, des.y) >= dis) {
+			dis = Distance(units[i].x, units[i].y, des.x, des.y);
+			farId = units[i].entityId;
+		}
+	}
+	return farId;
 }
 
-void Patrol(int pos, vector <Entity> list, int i)
+void Move(int x, int y, int thrust)
+{
+	cout<<"MOVE "<<x<<" "<<y<<" "<<thrust<<endl;
+}
+
+void Spell(int mp, int Id, Pos des)
+{
+	cout << "WINGARDIUM "<<Id<<" "<<des.x<<" "<<des.y<<" "<<(int)(mp * 1)<< endl;
+}
+
+void Throw(Pos des, int power)
+{
+	cout<<"THROW "<<des.x<<" "<<des.y<<" "<<power<< endl;
+}
+
+void Patrol(Pos start, Pos end, Pos wizard, int thrust)
 {
 	static int flag = 0;
-	if (pos == 0) {
-		if (flag == 0){
-			if (list[i].y <= 2400) {
-				cout << "MOVE 750 5400 150" << endl;
-			}
-			else if (list[i].y > 2400 && list[i].y <= 5100) {
-				cout << "MOVE 750 5400 150" << endl;
-			}
-			else if (list[i].y > 5100) {
-				cout << "MOVE 750 5400 50" << endl;
-				flag = 1;
-			}
-		}
-		else {
-			if (list[i].y <= 2400) {
-				cout << "MOVE 750 1400 50" << endl;
-				flag = 0;
-			}
-			else if (list[i].y > 2400 && list[i].y <= 5100) {
-				cout << "MOVE 750 1400 150" << endl;
-			}
-			else if (list[i].y > 5100) {
-				cout << "MOVE 750 1400 150" << endl;
-			}
+	double dis_from_start = Distance(start.x, start.y, wizard.x, wizard.y);
+	double dis_from_end = Distance(end.x, end.y, wizard.x, wizard.y);
+	if (flag == 0) {
+		if (dis_from_end > POLE_R)
+			Move(end.x, end.y, thrust);
+		else if (dis_from_end <= POLE_R) {
+			Move(start.x, start.y, (int)(thrust * 0.8));
+			flag = 1;
 		}
 	}
-	else {
-		if (flag == 0){
-			if (list[i].y <= 2400) {
-				cout << "MOVE 15250 5400 150" << endl;
-			}
-			else if (list[i].y > 2400 && list[i].y <= 5100) {
-				cout << "MOVE 15250 5400 150" << endl;
-			}
-			else if (list[i].y > 5100) {
-				cout << "MOVE 15250 5400 50" << endl;
-				flag = 1;
-			}
-		}
-		else {
-			if (list[i].y <= 2400) {
-				cout << "MOVE 15250 1400 50" << endl;
-				flag = 0;
-			}
-			else if (list[i].y > 2400 && list[i].y <= 5100) {
-				cout << "MOVE 15250 1400 150" << endl;
-			}
-			else if (list[i].y > 5100) {
-				cout << "MOVE 15250 1400 150" << endl;
-			}
+	if (flag == 1) {
+		if (dis_from_start > POLE_R)
+			Move(start.x, start.y, thrust);
+		else if (dis_from_start <= POLE_R) {
+			Move(end.x, end.y, (int)(thrust * 0.8));
+			flag = 0;
 		}
 	}
 }
